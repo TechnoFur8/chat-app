5ba96366bb9197d890b9e02779eea48e4881239c
Nikita <nikitaloginovxxx1986@gmail.com>
2025-07-25 21:28:25 +0300

    create docker

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -9,9 +9,8 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "none",
-        secure: true,
-        domain: "chat-app-server-production-f321.up.railway.app"
+        sameSite: "Strict",
+        secure: false,
     })
 
     return token
